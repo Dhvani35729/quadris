ecf257304ddd1c02fe20eabb45cc63da64ad3a32
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2020-08-01 15:17:41 -0600

    make command autocomplete better

--- interpreter.cpp
+++ interpreter.cpp
@@ -3,7 +3,7 @@
 
 #include <iostream>
 #include <string>
-#include <cassert>
+#include <vector>
 
 Interpreter::Interpreter()
 {
@@ -30,6 +30,52 @@ void Interpreter::resetCommand()
     this->commands_.clear();
 }
 
+Command completer(std::string rawCommand)
+{
+    std::vector<std::string> commandNames = {
+        "left",
+        "right",
+        "down",
+        "clockwise",
+        "counterclockwise",
+        "drop",
+        "levelup",
+        "leveldown",
+        "norandom",
+        "random",
+        "sequence",
+        "restart",
+        "hint",
+        "I",
+        "J",
+        "L",
+        "S",
+        "Z",
+        "O",
+        "T",
+        "exit",
+        "ai",
+    };
+    std::vector<Command> options;
+    for (int i = 0; i < commandNames.size(); i++)
+    {
+        if (commandNames[i].rfind(rawCommand, 0) == 0)
+        {
+            // commandNames[i] starts with rawCommand
+            options.push_back((Command)i);
+        }
+    }
+
+    if (options.size() == 1)
+    {
+        return options.front();
+    }
+    else
+    {
+        return BAD_COMMAND;
+    }
+}
+
 std::istream &operator>>(std::istream &in, Interpreter &i)
 {
 
@@ -41,105 +87,18 @@ std::istream &operator>>(std::istream &in, Interpreter &i)
     {
         i.addCommand(EXIT);
     }
-    else if (cmd == "left" || cmd == "lef" || cmd == "l") // l is not supposed to be supported (cuz levelup/leveldown). added for convenience, delete later
-    {
-        i.addCommand(LEFT);
-    }
-    else if (cmd == "right" || cmd == "ri" || cmd == "rig" || cmd == "righ" || cmd == "right" || cmd == "r") // r is not supported, added for convenience, delete later
-    {
-        i.addCommand(RIGHT);
-    }
-    else if (cmd == "down" || cmd == "do" || cmd == "dow")
-    {
-        i.addCommand(DOWN);
-    }
-    else if (cmd == "drop" || cmd == "dr" || cmd == "dro")
-    {
-        i.addCommand(DROP);
-    }
-    else if (cmd == "clockwise" || cmd == "cl" || cmd == "clo" || cmd == "cloc" ||
-             cmd == "clock" || cmd == "clockw" || cmd == "clockwi" || cmd == "clockwis" || cmd == "cw") // idk if cw is supported but it is now
-    {
-        i.addCommand(CLOCKWISE);
-    }
-    else if (cmd == "counterclockwise" || cmd == "co" || cmd == "cou" || cmd == "coun" || cmd == "count" ||
-             cmd == "counte" || cmd == "counter" || cmd == "counterc" || cmd == "countercl" || cmd == "counterclo" ||
-             cmd == "countercloc" || cmd == "counterclock" || cmd == "counterclockw" || cmd == "counterclockwi" ||
-             cmd == "counterclockwis" || cmd == "ccw") // idk if ccw is supported but it is now
-    {
-        i.addCommand(COUNTERCLOCKWISE);
-    }
-    else if (cmd == "random" || cmd == "ra" || cmd == "ran" || cmd == "rand" || cmd == "rando") //Relevant only during levels 3 and 4, this command restores randomness in these levels.
-    {
-        i.addCommand(RANDOM);
-    }
-    else if (cmd == "norandom file" || cmd == "n" || cmd == "no" || cmd == "nor" || cmd == "nora" || cmd == "noran" ||
-             cmd == "norand" || cmd == "norando" || cmd == "norandom" || cmd == "norandom " || cmd == "norandom f" ||
-             cmd == "norandom fi" || cmd == "norandom fil") //Relevant only during levels 3 and 4, this command restores randomness in these levels.
-    {
-        i.addCommand(NO_RANDOM_FILE);
-    }
-    else if (cmd == "levelup" || cmd == "levelu" || cmd == "lu") // lu not supported, abbreviated for convenience
-    {
-        i.addCommand(LEVEL_UP);
-    }
-    else if (cmd == "leveldown" || cmd == "leveld" || cmd == "ld") // ld not supported, abbreviated for convenience
-    {
-        i.addCommand(LEVEL_DOWN);
-    }
-    else if (cmd == "restart" || cmd == "re" || cmd == "res" || cmd == "rest" || cmd == "resta" || cmd == "restar")
-    {
-        i.addCommand(RESTART);
-    }
-    else if (cmd == "hint" || cmd == "h" || cmd == "hi" || cmd == "hin")
-    {
-        i.addCommand(HINT);
-    }
-    else if (cmd == "sequence file" || cmd == "s" || cmd == "se" || cmd == "seq" || cmd == "sequ" || cmd == "seque" ||
-             cmd == "sequen" || cmd == "sequenc" || cmd == "sequence" || cmd == "sequence " || cmd == "sequence f" ||
-             cmd == "sequence fi" || cmd == "sequence fil")
-    {
-        i.addCommand(SEQUENCE_FILE);
-    }
-    else if (cmd == "I")
-    {
-        i.addCommand(I);
-    }
-    else if (cmd == "J")
-    {
-        i.addCommand(J);
-    }
-    else if (cmd == "L")
-    {
-        i.addCommand(L);
-    }
-    else if (cmd == "S")
-    {
-        i.addCommand(S);
-    }
-    else if (cmd == "Z")
-    {
-        i.addCommand(Z);
-    }
-    else if (cmd == "O")
-    {
-        i.addCommand(O);
-    }
-    else if (cmd == "T")
-    {
-        i.addCommand(T);
-    }
-    else if (cmd == "ai")
-    {
-        i.addCommand(BOT_MODE);
-    }
     else
     {
-        i.addCommand(BAD_COMMAND);
+        Command parsedCommand = completer(cmd);
+        if (parsedCommand == BAD_COMMAND)
+        {
+            i.addCommand(BAD_COMMAND);
+        }
+        else
+        {
+            i.addCommand(parsedCommand);
+        }
     }
-
-    // TODO: Remove
-    // assert(!in.fail());
 
     return in;
 }
